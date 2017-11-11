842b14fb195a0d16a00081a8ce4c5bf2c78822ad
Tolulope <tolesy2003@outlook.com>
2017-11-11 07:00:07 +0200

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace LocalDbsample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SkypletEntities db = new SkypletEntities();
+            var customers = db.Customers;
+            string result = "";
+            foreach (var customer in customers)
+            {
+                result += string.Format("{0} | {1} | {2}", customer.Name, customer.CustomerId, customer.Address);
+            }
+            resultLabel.Text = result;
+        }
+    }
+}
